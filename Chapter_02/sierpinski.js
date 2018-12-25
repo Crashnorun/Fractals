4c083e93a5e01640ef3dd26535f34e2b977488e5
Crashnorun <charlie.portelli@gmail.com>
2018-12-24 23:28:02 -0500

    Added color and shifting

--- Chapter_02/sierpinski.js
+++ Chapter_02/sierpinski.js
@@ -12,16 +12,20 @@ window.onload = function () {
         document.body.addEventListener("keyup", keyUp);
 
         function keyUp(event) {
-            console.log(event.keyCode);
+            console.log("Key pressed: " + event.keyCode);
             switch (event.keyCode) {
                 case 32:    // space bar
                     maxDepth += 1;
-                    draw();
+                    draw(false);
                     break;
 
                 case 80:    // p button
                     chaos.popImage();
                     break;
+
+                case 82:    // r button adds random translation
+                    maxDepth += 1;
+                    draw(true);
 
                 default:
                     break;
@@ -29,35 +33,43 @@ window.onload = function () {
         }
     }
 
-    function draw() {
+    function draw(bool) {
         chaos.clear();
         chaos.context.save();
         chaos.context.translate(chaos.width * 0.5, chaos.height * 0.6);
         chaos.context.scale(size, size);
-        drawTriangle(maxDepth);
+        drawTriangle(maxDepth, bool);
         chaos.context.restore()
     }
 
-    function drawTriangle(depth) {
+    function drawTriangle(depth, bool) {
+  
         let angle = -Math.PI / 2;
+        let x = bool ? Math.random() : 0;
+        let y = bool ? Math.random() : 0;
+        let r = Math.random() * 256;
+        let g = Math.random() * 256;
+        let b = Math.random() * 256;
+
         if (depth === 0) {
             chaos.context.beginPath();
 
             // move to top point of triangle
-            chaos.context.moveTo(Math.cos(angle), Math.sin(angle));
+            chaos.context.moveTo(Math.cos(angle) + x, Math.sin(angle) + y);
             angle += Math.PI * 2 / 3;
 
             // draw line to lower right point
-            chaos.context.lineTo(Math.cos(angle), Math.sin(angle));
+            chaos.context.lineTo(Math.cos(angle) + x, Math.sin(angle) + y);
 
             // draw line to final point
             angle += Math.PI * 2 / 3;
-            chaos.context.lineTo(Math.cos(angle), Math.sin(angle));
+            chaos.context.lineTo(Math.cos(angle) + x, Math.sin(angle) + y);
 
             // fill will close the shape
+            chaos.context.fillStyle = "rgb(" + r + "," + g + "," + b + ")"
             chaos.context.fill();
         } else {
-
+            //#region 
             // // draw top of triangle
             // chaos.context.save();
             // chaos.context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
@@ -80,13 +92,14 @@ window.onload = function () {
             // chaos.context.scale(0.5, 0.5);
             // drawTriangle(depth - 1);
             // chaos.context.restore();
+            //#endregion
 
             for (let i = 0; i < 3; i++) {
                 angle += Math.PI * 2 / 3;
                 chaos.context.save();
                 chaos.context.translate(Math.cos(angle) * 0.5, Math.sin(angle) * 0.5);
                 chaos.context.scale(0.5, 0.5);
-                drawTriangle(depth - 1);
+                drawTriangle(depth - 1, bool);
                 chaos.context.restore();
             }
         }
