d62d2e0f94ba9ef12b8297c881831f8496b0e36d
crashnorun <charlie.portelli@gmail.com>
2019-01-22 18:00:57 -0500

    started chapter 3

---
+++ Chapter_03/Circles_01.js
@@ -0,0 +1,71 @@
+window.onload = function () {
+
+    let maxDepth = 0,
+        numShapes = 3,
+        angles = [0, Math.PI * 2 / 3, Math.PI * 4 / 3],
+        size = 0,
+        dist = 0,
+        scaleFactor = 0.6;
+
+    init();
+
+    function init() {
+        chaos.init();
+        size = chaos.height / 8;
+        dist = size * 1.5;
+
+        draw();
+
+
+        document.body.addEventListener("keyup", keyup);
+
+        function keyup(event) {
+            console.log(event.keyCode);
+            switch (event.keyCode) {
+                case 32: // space
+                    maxDepth += 1;
+                    draw();
+                    break;
+
+                case 80: // p
+                    chaos.popImage();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        function draw() {
+            chaos.clear();
+            chaos.context.save();
+            chaos.context.translate(chaos.width * 0.5, chaos.height * 0.5);
+            drawShape();
+            iterate(maxDepth);
+            chaos.context.restore();
+        }
+
+
+        function iterate(depth) {
+            for (let i = 0; i < numShapes; i += 1) {
+                chaos.context.save();
+                chaos.context.rotate(angles[i]);
+                chaos.context.translate(dist, 0);
+                chaos.context.scale(scaleFactor, scaleFactor);
+
+                drawShape();
+                if (depth > 0)
+                    iterate(depth - 1);
+
+                chaos.context.restore();
+            }
+        }
+
+        function drawShape() {
+            chaos.context.fillStyle = "rgba(0,0,0,0.5)";
+            chaos.context.beginPath();
+            chaos.context.arc(0, 0, size, 0, Math.PI * 2, false);
+            chaos.context.fill();
+        }
+    }
+}
